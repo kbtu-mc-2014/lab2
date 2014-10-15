035ae5863831bb314f1b881e4e1b081cbcaba6c8
ilzhan <dmx-stap@mail.ru>
2014-10-16 01:22:27 +0600

    added lab2

---
+++ 11BD02042/Lab2mc/src/ilzhan/lab2mc/MainActivity.java
@@ -0,0 +1,76 @@
+package ilzhan.lab2mc;
+
+import ilzhan.lab2mc.Sub_Frag.subEventListener;
+import ilzhan.lab2mc.Title_Frag.titleEventListener;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity implements titleEventListener, subEventListener{
+
+		Cont_Frag cont_frag = new Cont_Frag();
+		Sub_Frag sub_frag = new Sub_Frag();
+		Title_Frag title_frag = new Title_Frag();
+		int titlePos = 0;
+		int subTitlePos = 0;
+		boolean withContent = true;
+ 
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+		  super.onCreate(savedInstanceState);
+		  setContentView(R.layout.main);
+		  Title_Frag title = new Title_Frag();
+		  getFragmentManager().beginTransaction().replace(R.id.frameTS, title).commit();
+		  /*if (savedInstanceState.getInt("titlePos") != 0) {
+		    titlePos = savedInstanceState.getInt("titlePos");
+		    showSubTitle(titlePos);
+		  }*/
+		  if (savedInstanceState!= null) subTitlePos = savedInstanceState.getInt("subTitlePos");
+		  withContent = (findViewById(R.id.frameC) != null);
+		  if (withContent) showContent(subTitlePos);
+		}
+		
+		void showContent(int pos) {
+			Log.e("SHOWING CONT", "position = "+ pos);
+			if (withContent) {
+			    Cont_Frag content = (Cont_Frag) getFragmentManager().findFragmentById(R.id.frameC);
+			    Title_Frag title = new Title_Frag();
+		        if (content == null || content.getPosition() != pos) {
+		          content = Cont_Frag.newInstance(pos);
+		          getFragmentManager().beginTransaction().replace(R.id.frameTS, title).commit();
+		          getFragmentManager().beginTransaction().replace(R.id.frameC, content).commit();
+		        }
+	        } else {
+	            startActivity(new Intent(this, Cont_Activity.class).putExtra("position", subTitlePos));      
+	        }
+		}
+		
+		void showSubTitle(int pos) {
+			Log.e("SHOWING SUB", "position = "+ pos);
+			Sub_Frag subtitles = Sub_Frag.newInstance(pos);
+			getFragmentManager().beginTransaction().replace(R.id.frameTS, subtitles).commit();
+			Log.e("SHOWING SUB IS END", "position = "+ pos);
+		}
+		
+		@Override
+		public void subClickEvent(int pos) {
+			this.subTitlePos = pos;
+			showContent(pos);
+			Log.e("TITLE CLICKED", "position = "+ pos);
+		}
+		
+		@Override
+		public void titleClickEvent(int pos) {
+			this.titlePos = pos;
+			showSubTitle(pos);
+			Log.e("TITLE CLICKED", "position = "+ pos);
+		}
+		
+		protected void onSaveInstanceState(Bundle outState) {
+		    super.onSaveInstanceState(outState);
+		    outState.putInt("titlePos", titlePos);
+		    outState.putInt("subTitlePos", subTitlePos);
+		}
+}
